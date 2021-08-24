87b9987d1a7e143ae6d2643577cd0fc2ed262146
Shaun Savage <swansavage@gmail.com>
2021-08-24 14:24:07 -0500

    setup formspree, favicon

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -33,7 +33,11 @@ const Contact = ({ data }) => {
             </p>
           </article>
           <article>
-            <form className="form contact-form">
+            <form
+              className="form contact-form"
+              action="https://formspree.io/f/mbjqpedo"
+              method="POST"
+            >
               <div className="form-row">
                 <label htmlFor="name">Your Name</label>
                 <input type="text" name="name" id="name" />
