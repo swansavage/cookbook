e4fc2e56b23d14bdd26a7368814371a54df24390
Shaun Savage <swansavage@gmail.com>
2021-08-24 17:32:38 -0500

    gatsby img fluid max-width setting for performance

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -16,6 +16,7 @@ const query = graphql`
         }
         image {
           gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+          fluid(maxWidth: 1000)
         }
       }
     }
